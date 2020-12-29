ef14c2152e29eebb1feb64fb3c75a04cd3c0bcef
MestreALMO <lusegardis@gmail.com>
2020-12-28 23:06:47 -0300

    Installed cors, so we can access it with the front-end

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
