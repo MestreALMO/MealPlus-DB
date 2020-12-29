ca0093325824ac155a8faebb82ef6c00888f24f8
MestreALMO <lusegardis@gmail.com>
2020-12-28 22:57:26 -0300

    User session working

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -17,13 +18,17 @@ class CreateUserService {
       throw new Error('Email address already used');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
+
+    delete user.password;
 
     return user;
   }
